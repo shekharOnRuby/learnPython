ef105c172c1a0ea95bd75f9e901529c9d6c58d5a
Shekhar karande <shekhar.karande@gmail.com>
2016-03-18 12:51:53 +0000

    updating the first 3 exercises

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#A coommetn, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print "I could have code like this" # and the comment after it is ignored
+
+# you can also use a commetn to "disable" or comment out a piece of code:
+#print "This wont run"
+
+print "This will run"
