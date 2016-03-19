4c181915d362ed781c48deb8008abbb0c0a20eaa
Shekhar Karande <shekhar.karande@gmail.com>
2016-03-19 21:22:39 +0000

    Committing everything text related

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#Here's some strange stuff, remember to type it exatly
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\n"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+    There is something going on here.
+    With the three double quotes.
+    We'll be able to type as much as we like.
+    Even 4 lines if want, or 5, or 6.
+
+"""
