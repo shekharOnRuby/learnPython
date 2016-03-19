a98ab68fa4d08cbec2ba3bbfff76b23ef4243543
Shekhar Karande <shekhar.karande@gmail.com>
2016-03-19 23:43:58 +0000

    pushing lessons for arv

---
+++ ex13.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+#
+
+script, first, second, third, fourth  = argv
+
+#script = raw_input()
+first = raw_input()
+second = raw_input()
+third = raw_input()
+
+
+print "The script variable is ", script
+print "The first variable is ", first
+print "The second variable is ", second
+print "The third variable is ", third
+print "The fourth variable is ", fourth
