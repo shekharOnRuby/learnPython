c666aaddd4d2495b5a0c75d2e5db56259b8098db
Shekhar Karande <shekhar.karande@gmail.com>
2016-03-19 15:29:53 +0000

    additional exercises

--- ex5.py
+++ ex5.py
@@ -1,17 +1,23 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie
-my_height = 75 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
-
-print "Let's talk about %s" %my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 75 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+celsius = 32
+farhenheit = (celsius * (9/2)) + 32
+km = 1
+mile = 0.625 * km
+print "Let's talk about %s" % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Atually thats not too heavy"
-print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the cofee." %my_teeth
+print "He's got %s eyes and %s hair" % (eyes, hair)
+print "His teeth are usually %s depending on the cofee." %teeth
 
 #this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d" %(my_age,my_height, my_weight, my_age + my_weight + my_height)
+print "If I add %d, %d, and %d I get %d" %(age,height,weight, age + weight + height)
+
+print " %d celsius converst to %f farhenheit" %(celsius, farhenheit)
+print " And %d km is %f miles" %(km, mile)
