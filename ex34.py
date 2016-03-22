289437aa8d72325340bae0ded09b61fbc7246806
Shekhar Karande <shekhar.karande@gmail.com>
2016-03-22 12:52:41 +0000

    push exercises for loopsgit status

---
+++ ex34.py
@@ -0,0 +1,14 @@
+animals = ['bear','python','peacock','kangaroo', 'whale', 'platypus']
+
+print animals
+print "The animal at 1 %s" % animals[1]
+print "The third(3rd) animal. %s ", animals[2]
+print "The first animal %s", animals[0]
+print "The anmial at 3 %s ", animals[3]
+print "The fifht(5th) animal. %s", animals[4]
+print "The anmial at 2. %s",animals[2]
+print "The sixth(6th) animal %s", animals[5]
+print "The animal at 4 %s", animals[4]
+
+for a in animals:
+    print a[2]
